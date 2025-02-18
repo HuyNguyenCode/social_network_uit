37c86c16495e0ce9a221e2986149c1793e6854b9
HuyNguyenCode <huyprolk111@gmail.com>
2025-02-18 13:01:31 +0700

    [UI] UI update

--- src/app/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-import ProfilePage from "@/app/settings/profile";
-import TodoPage from "@/app/settings/todo";
-export default function Settings() {
-  return (
-    // <ProfilePage/>
-    <TodoPage/>
-  );
-}
